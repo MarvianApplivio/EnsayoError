2d3041b5592308904ba609e1db43c46f5b313873
= <=>
2022-02-18 10:47:59 -0400

    subiendo cambios para actualizar

---
+++ apis/events-api/src/bussines/bo/status.bo.ts
@@ -0,0 +1,10 @@
+import { StatusRepository } from "../../repository/status.repository";
+import { BaseBo } from "./abstractBase.bo";
+
+export class StatusBo extends BaseBo<StatusRepository> {
+
+    constructor() {
+        super()
+    }
+
+}
